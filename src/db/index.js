eeaf7adb501c189b08cdbe46ddffc091840e4845
Lucas Falcone <lucasfalcone841@gmail.com>
2026-06-11 15:34:03 -0300

    fix db connection using DATABASE_URL

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,24 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-})
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        ssl: { rejectUnauthorized: false },
+      }
+)
 
 pool.query('SELECT NOW()')
   .then(() => console.log('✅ Conectado a PostgreSQL'))
-  .catch(err => console.error('❌ Error PostgreSQL:', err));
+  .catch(err => console.error('❌ Error PostgreSQL:', err))
 
 module.exports = pool
